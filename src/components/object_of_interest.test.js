cb154f01cee8d886275801e89d24254c22bef7a5
Vitaly Arkhipov <c-vitaly.arkhipov@charter.com>
2020-09-30 17:14:00 -0500

    1st jest test implementd

--- src/components/object_of_interest.test.js
+++ src/components/object_of_interest.test.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import ObjectOfInterest from './objects_of_interest'
+import React from "react";
+import { render, unmountComponentAtNode } from "react-dom";
+import { act } from "react-dom/test-utils";
+import ObjectOfInterest from "./objects_of_interest";
+//import { fakeObjectOfInterest } from "./test-data/fakeObjectOfInterest";
 
 
-test('renders learn react link', () => {
-  const { getByText } = render(<ObjectOfInterest object_of_interest='car' url={{url:'http://m',cam:0}}/>);
- // const linkElement = getByText(/learn react/i);
- // expect(linkElement).toBeInTheDocument();
+ 
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("renders ObjectOfInterest data", async () => {
+  const fakeObjectOfInterest = [    { hashcode: 'ABCF23422323' , currenttime: 1601476686.8732808, frame: 'data:image/jpeg;base64'}
+  ]
+  jest.spyOn(global, "fetch").mockImplementation(() =>
+  Promise.resolve({
+    json: () => fakeObjectOfInterest
+  })
+);
+
+// Use the asynchronous version of act to apply resolved promises
+await act(async () => {
+  render(<ObjectOfInterest start={0} end={50} cam={0} object_of_interest={'car'} />, container);
+});
+
+expect(container.textContent).toContain('loading...');
+
+// remove the mock to ensure tests are completely isolated
+global.fetch.mockRestore();
 });
