b0176fd523fdd78226e8f1674feee3774edb0b33
zeroprg <zeroprg@yahoo.com>
2020-10-14 13:49:14 +0000

    shell

--- production.sh
+++ production.sh
@@ -2,9 +2,10 @@
 
 cd /home/odroid/projects/ai-ui-component &&
 git pull  --no-edit
-npm install &&
+npm install
+echo $1 | sudo -S -k rm -rf build
 npm run build &&
 echo $1 | sudo -S -k npm install -g build
-pkill -f serve  &&
+pkill -f serve 
 serve -s build &
 
